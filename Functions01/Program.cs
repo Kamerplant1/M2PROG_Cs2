089eb0063dd91fa62d541ea8fb09719062fb7464
Tristan Wals <39040@ma-web.nl>
2024-11-18 12:23:21 +0100

    updated opdracht

--- Functions01/Program.cs
+++ Functions01/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Functions01
+﻿using System.Runtime.CompilerServices;
+
+namespace Functions01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program*program*=*new*program();
+            program.Run();
+            //Program is de variable
+            //Type is int
+            //Program is type 
+            //return type is void
+            
+            interal*void*Run()
+                Console.WriteLine("Dit is nu het start van mijn programma")
         }
     }
 }
